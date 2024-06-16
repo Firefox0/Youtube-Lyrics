f22723656f20270bc7d87274d13eff0305781ea0
Your Name <you@example.com>
2024-06-16 13:54:06 +0200

    Refactoring + added path check

--- tools/packaging.py
+++ tools/packaging.py
@@ -3,33 +3,50 @@ import zipfile
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("platform", choices=["firefox", "chrome"])
-args = parser.parse_args()
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("platform", choices=["firefox", "chrome"])
+    return parser.parse_args()
 
-source = ""
-output = ""
-if args.platform == "firefox":
-    source = "manifestFirefoxV2.json"
-    output = "firefoxPackage.zip"
-else:
-    source = "manifestChromeV3.json"
-    output = "chromePackage.zip"
+def create_zip(paths, output):
+    with zipfile.ZipFile(output, "w") as f:
+        for path in paths:
+            if not os.path.exists(path):
+                print(f"Path '{path}' does not exist. Exiting.")
+                return False
 
-shutil.copyfile(source, "manifest.json")
-if (os.path.exists(output)):
-    os.remove(output)
+            if os.path.isdir(path):
+                for root, dirs, files in os.walk(path):
+                    for d in dirs:
+                        f.write(os.path.join(root, d))
+                    for file in files:
+                        f.write(os.path.join(root, file))
+            
+            f.write(path)
+    
+    return True
 
-l = ["dist", "src/css/styles.css", "manifest.json"]
-with zipfile.ZipFile(output, "w") as f:
-    for e in l:
-        if os.path.isdir(e):
-            for root, dirs, files in os.walk(e):
-                for d in dirs:
-                    f.write(os.path.join(root, d))
-                for file in files:
-                    f.write(os.path.join(root, file))
-        
-        f.write(e)
+def main():
+    source = ""
+    output = ""
 
-os.remove("manifest.json")
+    args = get_args()
+    if args.platform == "firefox":
+        source = "manifestFirefoxV2.json"
+        output = "firefoxPackage.zip"
+    else:
+        source = "manifestChromeV3.json"
+        output = "chromePackage.zip"
+
+    shutil.copyfile(source, "manifest.json")
+    if (os.path.exists(output)):
+        os.remove(output)
+
+    paths = ["dist", "src/css/styles.css", "manifest.json"]
+    if not create_zip(paths, output):
+        os.remove(output)
+
+    os.remove("manifest.json")
+
+if __name__ == "__main__":
+    main()
